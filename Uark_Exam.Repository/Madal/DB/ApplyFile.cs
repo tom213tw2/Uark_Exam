8fc02ee6c3a42848258c84de9370429832225e42
高宗毅 <b98170005@gmail.com>
2023-09-14 20:14:22 +0800

    Add Repository class library and add Repository reference

---
+++ Uark_Exam.Repository/Madal/DB/ApplyFile.cs
@@ -0,0 +1,33 @@
+using System;
+using System.ComponentModel;
+using Dapper;
+
+namespace Uark_Exam.Repository.Madal.DB
+{
+    [Table("Apply_File", Schema = "dbo")]
+    public class ApplyFile
+    {
+        [Key]
+        [Required]
+        [Column("Id")]
+        [DisplayName("Id")]
+        public Guid Id { get; set; }
+    
+        [Required]
+        [Column("user_id")]
+        [DisplayName("UserId")]
+        public Guid UserId { get; set; }
+    
+        [Column("file_path")]
+        [DisplayName("檔案路徑")]
+        public string FilePath { get; set; }
+    
+        [Column("created_at")]
+        [DisplayName("建立日期")]
+        public DateTime CreatedDateTime { get; set; }
+    
+        [Column("updated_at")]
+        [DisplayName("異動日期")]
+        public DateTime UpdatedDateTime { get; set; }
+    }
+}
